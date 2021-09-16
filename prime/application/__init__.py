d2a30e09bf7e018072630091ec6a2d10ba8d5d09
nathanforester <nathanforester32@gmail.com>
2021-09-16 17:25:05 +0100

    major changes to codebase

--- prime/application/__init__.py
+++ prime/application/__init__.py
@@ -1,16 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response 
 
 app = Flask(__name__)
 
-@app.route('/<dob>')
-def prime(dob):
-    if dob > 1:
-        if dob % 2 == 1 or dob == 2:
+@app.route('/prime/<int:birthDate>', methods=['GET', 'POST'])
+def prime(birthDate):
+    if birthDate > 1:
+        if birthDate % 2 == 1 or birthDate == 2:
             prime = "PRIME number"
-        elif dob % 2 != 1:
+        elif birthDate % 2 != 1:
             prime = "COMPOSITE number"
         else:
             prime = "You do not appear to exist"
     else:
         prime = "You do not appear to exist"
-    return jsonify(prime) 
+    return Response(jsonify(str(prime))) 
