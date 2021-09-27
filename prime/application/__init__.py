c245c47f589d2db412f029b0e3196926db8691bd
nathanforester <nathanforester32@gmail.com>
2021-09-27 16:41:38 +0100

    logic does work, issues displaying proper result in html

--- prime/application/__init__.py
+++ prime/application/__init__.py
@@ -1,18 +1,21 @@
 from flask import Flask, jsonify, Response 
+import sys
 
 app = Flask(__name__)
 
-@app.route('/prime/<ageInMonths>', methods=['GET', 'POST'])
+@app.route('/date/<ageInMonths>', methods=['GET', 'POST'])
 def prime(ageInMonths):
-
-    if int(ageInMonths) > 1:
-        if int(ageInMonths) % 2 == 1 or int(ageInMonths) == 2:
-            prime = "PRIME number"
-        elif int(ageInMonths) % 2 != 1:
-            prime = "COMPOSITE number"
+     
+    ageInMonths = int(ageInMonths)
+    if ageInMonths > 1:
+        if ageInMonths % 2 == 1 or ageInMonths == 2:
+            prime = "yes, you are PRIME"
+        elif ageInMonths % 2 != 1:
+            prime = "No, you are COMPOSITE"
         else:
             prime = "You do not appear to exist"
     else:
         prime = "You do not appear to exist"
-    
-    return str(prime)
+    print(str(prime), file=sys.stderr)
+    print(str(prime), file=sys.stdout)
+    return Response(str(prime))
