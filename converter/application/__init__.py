60138e435e48738103754163d32609959acf374b
nathanforester <nathanforester32@gmail.com>
2022-03-23 17:07:15 +0000

    working code

--- converter/application/__init__.py
+++ converter/application/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify, Response 
 import sys
+from flask import jsonify
+from flask import jsonify
+import json
+from flask import json
+
 
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/date/<birthDates>', methods=['GET', 'POST'])
-def birthDate(birthDates):
-    birthDates = int(birthDates)  
+@app.route('/date/<birthDate>', methods=['GET', 'POST'])
+def birthDate(birthDate):
+    
+    birthDate = int(float(birthDate))  
     year = datetime.utcnow().year
-    ageInMonths = (int(year) - int(birthDates)) * 12
-    print(str(ageInMonths), file=sys.stderr)
-    print(str(ageInMonths), file=sys.stdout)
-    return Response(str(ageInMonths))
+    ageInMonths = (int(year) - int(birthDate)) * 12
+    return str(ageInMonths)
--- prime/application/__init__.py
+++ prime/application/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask, jsonify, Response 
+from flask import jsonify
 import sys
+from flask import jsonify
+import json
+from flask import json
 
 app = Flask(__name__)
 
 @app.route('/date/<ageInMonths>', methods=['GET', 'POST'])
 def prime(ageInMonths):
+    ageMonths = int(float(ageInMonths))
      
-    ageInMonths = int(ageInMonths)
-    if ageInMonths > 1:
-        if ageInMonths % 2 == 1 or ageInMonths == 2:
+    ageMonths = int(ageMonths)
+    if ageMonths > 1:
+        if ageMonths % 2 == 1 or ageMonths == 2:
             prime = "yes, you are PRIME"
-        elif ageInMonths % 2 != 1:
+        elif ageMonths % 2 != 1:
             prime = "No, you are COMPOSITE"
         else:
             prime = "You do not appear to exist"
@@ -18,4 +23,4 @@ def prime(ageInMonths):
         prime = "You do not appear to exist"
     print(str(prime), file=sys.stderr)
     print(str(prime), file=sys.stdout)
-    return Response(str(prime))
+    return prime
