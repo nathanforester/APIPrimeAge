bbfb78ed28381e6f03c82d607c41b9632cf0ea5b
Nathan Forester <Nathan.Forester@estio.co.uk>
2023-07-05 17:40:56 +0100

    updated

--- converter/application/routes.py
+++ converter/application/routes.py
@@ -10,13 +10,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
+
 @app.route('/date/<birthDate>', methods=['GET', 'POST'])
 def birthDate(birthDate):
-    message = 'value entered is less than one month'
     birthDate = int(float(birthDate))  
     year = datetime.utcnow().year
     ageInMonths = (int(year) - int(birthDate)) * 12
-    if ageInMonths < 1:
-        return message
+    if birthDate < 1:
+        return 'value entered is less than one month'
     else: 
         return str(ageInMonths)
